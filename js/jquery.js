0d8445ac56e455969adf23d09733befa1749d886
Shahriar Yusuf <1100shahriar1997@gmail.com>
2021-07-08 12:23:56 +0600

    first commit

---
+++ js/jquery.js
@@ -0,0 +1,8 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoicmFwdG9yOTciLCJhIjoiY2txdWd1MzhhMDRmbjJ3cG1qY2x1N3JoYSJ9.EJUrejqvIBvvmOitFDUDRw';
+var map = new mapboxgl.Map({
+container: 'map', // container ID
+style: 'mapbox://styles/mapbox/streets-v11', // style URL
+center: [91.83398765315798, 22.347178800666736], // starting position [lng, lat]
+zoom: 9 // starting zoom
+});
+
